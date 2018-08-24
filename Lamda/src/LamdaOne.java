ee99624ef66a1e1730e4b1e4702cd47ce79d866a
makansari78 <mak.ansari@rjtcompuquest.com>
2018-08-25 00:51:25 +0530

    Initial commit

---
+++ Lamda/src/LamdaOne.java
@@ -0,0 +1,19 @@
+
+interface Drawable{  
+	    public void draw();  
+	} 
+
+
+
+public class LamdaOne {
+
+	 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 final int width=10;  
+		  
+		 
+	        
+	}
+
+}
